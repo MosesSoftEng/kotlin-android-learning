b88950666eb51209bf8446df882105ca70a7a96a
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-06-15 20:00:48 +0300

    GADS Kotlin Developer Scholarship

---
+++ GADS2022/course-notes-app/app/src/main/java/com/gads2022/coursenotesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gads2022.coursenotesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
