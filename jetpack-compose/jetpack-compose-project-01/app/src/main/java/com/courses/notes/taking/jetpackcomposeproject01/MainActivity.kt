347efd941caf0e73d4702271d88b9d4a6a0dad36
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-07-07 16:01:26 +0300

    Add row and image view

--- jetpack-compose/jetpack-compose-project-01/app/src/main/java/com/courses/notes/taking/jetpackcomposeproject01/MainActivity.kt
+++ jetpack-compose/jetpack-compose-project-01/app/src/main/java/com/courses/notes/taking/jetpackcomposeproject01/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.compose.foundation.Image
 
 class MainActivity : ComponentActivity() {
     data class Message(val author: String, val body: String)
@@ -29,9 +33,16 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MessageCard(message: Message) {
         /* Set text for content block */
-        Column {
-            Text(text = message.author)
-            Text(text = message.body)
+        Row {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "Contact profile picture",
+            )
+
+            Column {
+                Text(text = message.author)
+                Text(text = message.body)
+            }
         }
     }
 
