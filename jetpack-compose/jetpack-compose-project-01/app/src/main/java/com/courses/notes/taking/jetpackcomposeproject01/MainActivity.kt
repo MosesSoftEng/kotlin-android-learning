fef508f57ead1c9a07fabc763c36c6441015a865
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-07-07 16:22:17 +0300

    Add modifiers to composable views

--- jetpack-compose/jetpack-compose-project-01/app/src/main/java/com/courses/notes/taking/jetpackcomposeproject01/MainActivity.kt
+++ jetpack-compose/jetpack-compose-project-01/app/src/main/java/com/courses/notes/taking/jetpackcomposeproject01/MainActivity.kt
@@ -3,14 +3,17 @@ package com.courses.notes.taking.jetpackcomposeproject01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     data class Message(val author: String, val body: String)
@@ -33,14 +36,28 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MessageCard(message: Message) {
         /* Set text for content block */
-        Row {
+        Row (
+            // Add padding using modifier
+            modifier = Modifier.padding(all = 8.dp)
+
+        ){
             Image(
                 painter = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp) // Image size
+                    .clip(CircleShape) // Image circle shape
             )
+
+            //  Add horizontal space between.
+            Spacer(modifier = Modifier.width(8.dp))
 
             Column {
                 Text(text = message.author)
+
+                //  Add vertical space between.
+                Spacer(modifier = Modifier.height(8.dp))
+
                 Text(text = message.body)
             }
         }
