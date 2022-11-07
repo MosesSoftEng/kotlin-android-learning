e1cf628dad24b53f5165da4a842191feafcd481b
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-07 20:56:53 +0300

    Ads query to get stop schedules

--- GADS2022/data-persistence-room-flow/app/src/main/java/database/schedule/ScheduleDao.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/database/schedule/ScheduleDao.kt
@@ -8,4 +8,8 @@ interface ScheduleDao {
     /*Get all schedules*/
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
     fun getAll(): List<Schedule>
+
+    /*Get all schedules for a given stop*/
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): List<Schedule>
 }
