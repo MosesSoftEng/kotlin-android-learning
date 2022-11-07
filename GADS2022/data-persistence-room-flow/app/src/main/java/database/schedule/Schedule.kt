35079d60eb1ec48c39f563b6afbd86536fb48d08
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-07 20:42:27 +0300

    Add stop_name column property

--- GADS2022/data-persistence-room-flow/app/src/main/java/database/schedule/Schedule.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/database/schedule/Schedule.kt
@@ -1,7 +1,12 @@
 package database.schedule
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.PrimaryKey
 
 data class Schedule(
-    @PrimaryKey val id: Int
+    @PrimaryKey val id: Int,
+    @NonNull @ColumnInfo(name = "stop_name") /*set column name*/
+    val stopName: String,
+    
 )
