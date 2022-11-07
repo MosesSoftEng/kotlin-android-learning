be118b57ad7158590ae37fa567cc36dd0ebfe81c
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-07 20:55:41 +0300

    Add query for schedules

--- GADS2022/data-persistence-room-flow/app/src/main/java/database/schedule/ScheduleDao.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/database/schedule/ScheduleDao.kt
@@ -1,2 +1,11 @@
 package database.schedule
 
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface ScheduleDao {
+    /*Get all schedules*/
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): List<Schedule>
+}
