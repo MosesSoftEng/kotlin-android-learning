5b1a15493986ccba6b14a4e901f07fbba175e338
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-08 12:29:54 +0300

    Add view Binding

--- GADS2022/data-persistence-room-flow/app/src/main/java/learn/data/persistence/room/flow/MainActivity.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/learn/data/persistence/room/flow/MainActivity.kt
@@ -2,12 +2,14 @@ package learn.data.persistence.room.flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import learn.data.persistence.room.flow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
