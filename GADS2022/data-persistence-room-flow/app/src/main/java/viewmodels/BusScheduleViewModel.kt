a8ee379072b75b1089d99e8ed1ddbe8821c36e12
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-08 05:40:20 +0300

    Add get schedule method

--- GADS2022/data-persistence-room-flow/app/src/main/java/viewmodels/BusScheduleViewModel.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/viewmodels/BusScheduleViewModel.kt
@@ -1,8 +1,9 @@
 package viewmodels
 
 import androidx.lifecycle.ViewModel
+import database.schedule.Schedule
 import database.schedule.ScheduleDao
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() /*inheritance*/{
-
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
 }
