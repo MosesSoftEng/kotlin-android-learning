a476850652773b0fe5ce1ccc1425694f8b983ff7
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-08 06:03:06 +0300

    Add create override method

--- GADS2022/data-persistence-room-flow/app/src/main/java/viewmodels/BusScheduleViewModel.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/viewmodels/BusScheduleViewModel.kt
@@ -12,4 +12,11 @@ class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() /*
 }
 
 class BusScheduleViewModelFactory(private val scheduleDao: ScheduleDao): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
