1c0d1569c0a12ecdc156bff3b7a638ec1055794a
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-08 05:39:25 +0300

    Update BusScheduleViewModel.kt

--- GADS2022/data-persistence-room-flow/app/src/main/java/viewmodels/BusScheduleViewModel.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/viewmodels/BusScheduleViewModel.kt
@@ -1,2 +1,8 @@
 package viewmodels
 
+import androidx.lifecycle.ViewModel
+import database.schedule.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() /*inheritance*/{
+
+}
