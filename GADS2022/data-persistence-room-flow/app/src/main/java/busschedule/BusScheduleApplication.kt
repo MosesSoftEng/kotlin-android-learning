d8c39ac6e0f5788864d1c5989b914281c059da0d
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-08 06:42:32 +0300

    Add method to return appdatabase instance

--- GADS2022/data-persistence-room-flow/app/src/main/java/busschedule/BusScheduleApplication.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/busschedule/BusScheduleApplication.kt
@@ -1,2 +1,10 @@
 package busschedule
 
+import android.app.Application
+import database.AppDatabase
+
+class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy {
+        AppDatabase.getDatabase(this)
+    }
+}
