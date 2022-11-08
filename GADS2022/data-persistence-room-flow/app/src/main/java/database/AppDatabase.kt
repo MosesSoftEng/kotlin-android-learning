ff75c07ae953736ad3eb1f6d3a4a9443d3ac2f5c
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-11-08 06:35:20 +0300

    Add method to get database instance

--- GADS2022/data-persistence-room-flow/app/src/main/java/database/AppDatabase.kt
+++ GADS2022/data-persistence-room-flow/app/src/main/java/database/AppDatabase.kt
@@ -1,2 +1,39 @@
 package database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import database.schedule.Schedule
+import database.schedule.ScheduleDao
+
+/**
+ * Manage models, DAO classes, and any database setup.
+ */
+@Database(entities = arrayOf(Schedule::class), version = 1/* For migration updates */)
+abstract class AppDatabase: RoomDatabase() {
+    /* Allow DAO classes access to other classes */
+    abstract fun scheduleDao(): ScheduleDao
+
+    /* Ensure single instance of this class */
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        /* Function to return AppDatabase instance */
+        fun getDatabase(context: Context): AppDatabase {
+            /* Elvis operator */
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database")
+                    .createFromAsset("database/bus_schedule.db") /* Repopulate data */
+                    .build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
+}
